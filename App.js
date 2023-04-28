6c86287295eb5d046af35ddad2124d2d90c8d313
Isigarcia0493 <ig705295@southeast.edu>
2023-04-28 11:52:37 -0500

    First Commit

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Pressable, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import * as SQLite from "expo-sqlite";
+import * as WebBrowser from 'expo-web-browser';
 import * as SplashScreen from 'expo-splash-screen';
 
 SplashScreen.preventAutoHideAsync();
 setTimeout(SplashScreen.hideAsync, 2000);
 
+function openDatabase() {
+  if (Platform.OS === "web") {
+    return {
+      transaction: () => {
+        return {
+          executeSql: () => {},
+        };
+      },
+    };
+  }
+
+  const db = SQLite.openDatabase("milesDB.db");
+  return db;
+}
+
+const db = openDatabase();
+
 
 const mainImage = require('./assets/mileage.jpg');
 
 function HomeScreen({ navigation }){
+
+  function handleUrl() {
+    WebBrowser.openBrowserAsync('https://www.tomkadleckia.com/blogs/1309/good-mileage-preowned-car/');
+    }
+
   return(
     <View style={styles.container}>
       <View style={styles.welcomeContainer}>
@@ -21,7 +45,7 @@ function HomeScreen({ navigation }){
       <View style={styles.imageContainer}>
         <Image source={mainImage} style={styles.image} />
       </View> 
-      <View style={styles.btnsContainer}>
+      <View style={styles.homeContainersBtns}>
         <Pressable
           onPress={() => {
             navigation.navigate("Calculate")
@@ -29,7 +53,21 @@ function HomeScreen({ navigation }){
           style={styles.calculateBtn}>
           <Text style={styles.calculateLabel}>Calculate</Text>
         </Pressable>
-      </View>           
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Your Records")
+          }}                   
+          style={styles.recordsBtn}>
+          <Text style={styles.recordsLabel}>Records</Text>
+        </Pressable>
+      </View>   
+      <View style={styles.btnsContainer}>
+        <Pressable
+          onPress={() => handleUrl()}                   
+          style={styles.recommendationsBtn}>
+          <Text style={styles.recommendationsLabel}>Recommendations</Text>
+        </Pressable>    
+      </View>    
       <StatusBar style="auto" />
     </View>
   );
@@ -41,6 +79,46 @@ function CalculationScreen ({ route, navigation }){
   const [gallons, setGallons] = useState(0);
   const [milesGallons, setMilesGallons] = useState(0);
   const [isDatePickerVisible, setDatePickerVisible] = useState(false);
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      // tx.executeSql(
+      //   "drop table MileageCalc;"
+      // );
+      tx.executeSql(
+        "create table if not exists MileageCalc (id integer primary key not null, miles real, purchaseDate text, gallons real, milesGallons real);"
+      );
+    })
+  }, []);
+
+  const add = (miles, date, gallons, milesGallons) => {
+    if (miles === null || miles === NaN || miles === ''){
+      return false;
+    }
+
+    if (date === null || date === ''){
+      return false;
+    }
+
+    if (gallons === null || gallons === NaN || gallons === ''){
+      return false;
+    }
+
+    if(milesGallons === null || milesGallons === NaN || milesGallons === ''){
+      return false;
+    }
+
+    console.log(milesGallons);
+    
+    db.transaction(
+      (tx) => {
+        tx.executeSql("insert into MileageCalc (miles, purchaseDate, gallons, milesGallons) values (?, ?, ?, ?)", [miles, date.toLocaleDateString(), gallons, milesGallons]);
+        tx.executeSql("select * from MileageCalc", [], (_, { rows }) =>
+          console.log(JSON.stringify(rows))
+        );
+      }
+    );
+  }
 
   const showDatePicker = () => {
     setDatePickerVisible(true);
@@ -74,10 +152,33 @@ function CalculationScreen ({ route, navigation }){
 
   const handleCalculateMiles = () =>{
     let total = 0;
+    let milesValid = false;
+    let dateValid = false;
+    let gallonsValid = false;
 
-    total = (miles / gallons).toFixed(2);
-    
-    setMilesGallons(total);   
+    if(miles == 0 || miles == null || miles == ""){
+      alert('Please enter miles driven')
+    }else{
+      milesValid = true;
+    }
+
+    if(date == 0 || date == null || date == ""){
+      alert('Please select a date')
+    }else{
+      dateValid = true;
+    }
+
+    if(gallons == 0 || gallons == null || gallons == ""){
+      alert('Please enter gallons purchased')
+    }else{
+      gallonsValid = true;
+    }
+
+    if(milesValid && dateValid && gallonsValid){
+      total = (miles / gallons).toFixed(2);
+      setMilesGallons(total);  
+      add(miles, date, gallons, total);
+    } 
   }
 
   return (
@@ -89,6 +190,7 @@ function CalculationScreen ({ route, navigation }){
             style={styles.input}
             onChangeText={onChangeMiles}
             value={miles ? miles : ""}
+            returnKeyType='done'
             keyboardType='numeric'
           />
         </View>
@@ -98,6 +200,7 @@ function CalculationScreen ({ route, navigation }){
             style={styles.inputDate}
             onChangeText={onChangeDate}
             value={getDate()}
+            returnKeyType='done'
             editable = {false}
           />
           <TouchableOpacity
@@ -119,6 +222,7 @@ function CalculationScreen ({ route, navigation }){
             style={styles.input}
             onChangeText={onChangeGallons}
             value={gallons ? gallons : ""}
+            returnKeyType='done'
             keyboardType='numeric'
           />
         </View>
@@ -146,10 +250,69 @@ function CalculationScreen ({ route, navigation }){
               </View>
         </View>
         : ""}
+        <View style={styles.btnsGoToRecordsContainer}>
+          <Pressable
+            onPress={() => {
+              navigation.navigate("Your Records")
+            }}                   
+            style={styles.goToRecordsBtn}>
+            <Text style={styles.goToRecordsLabel}>Go to Records</Text>
+          </Pressable>  
+        </View>   
       </View>
       <StatusBar style="auto" />
     </View>
   );
+}
+
+function RecordsScreen ({navigation}){
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        `select id, miles, purchaseDate, gallons, milesGallons from MileageCalc order by purchaseDate desc;`,
+        null,
+        (_, { rows: { _array } }) => setRecords(_array)
+      )
+
+    })
+  }, []);
+
+  return (
+    <View style={styles.recordsContainer}>      
+        <View style={styles.titleHeader}>
+          <Text style={styles.headerTable}>Date</Text>
+          <Text style={styles.headerTable}>       Miles</Text>
+          <Text style={styles.headerTable}>Gallons</Text>
+          <Text style={styles.headerTable}>MPG</Text>
+        </View>
+      <ScrollView>
+        {records != null || records != "" || records.length != 0 ?
+          <View style={styles.recordsContainer}>
+            {records.map(({ id, miles, purchaseDate, gallons, milesGallons }) => (
+            <View key={id} style={styles.recordsItemList}>   
+              <Text style={styles.recordsItems}>{purchaseDate}</Text>
+              <Text style={styles.recordsItems}>{miles}</Text>
+              <Text style={styles.recordsItems}>{gallons} G</Text>
+              <Text style={styles.recordsItems}>{milesGallons}</Text>
+            </View>
+            ))}
+          </View> 
+          : '' }
+      </ScrollView>
+      <View style={styles.calculatorBtnContainer}>
+          <Pressable
+            onPress={() => {
+              navigation.navigate("Calculate")
+            }}                   
+            style={styles.goToRecordsBtn}>
+            <Text style={styles.goToRecordsLabel}>Calculator</Text>
+          </Pressable>  
+        </View>   
+    </View>
+  )
 }
 
 const Stack = createNativeStackNavigator();
@@ -164,6 +327,9 @@ export default function App() {
         <Stack.Screen name="Calculate" component={CalculationScreen} options={{ 
                         headerStyle: { backgroundColor: '#D9D9D9'},
                         headerTintColor: '#000'}} />
+        <Stack.Screen name="Your Records" component={RecordsScreen} options={{ 
+                        headerStyle: { backgroundColor: '#D9D9D9'},
+                        headerTintColor: '#000'}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -176,12 +342,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  recordsContainer: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
   calculationContainer: {
     flex: 1,
     backgroundColor: '#000000',
   },
+  calculatorBtnContainer: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    alignItems: 'center',
+  },
   welcomeContainer: {
-    paddingBottom: 30,
+    paddingBottom: 45,
+    paddingTop: 15,
   },
   welcome: {
     color: "#FFFFFF",
@@ -197,7 +373,11 @@ const styles = StyleSheet.create({
   },
   btnsContainer: {
     flex: 0.8,
-    paddingTop: 80
+    paddingTop: 50
+  },
+  btnsGoToRecordsContainer: {
+    paddingTop: 20,
+    alignItems: 'center',
   },
   calculateBtn:{
     width: 151,
@@ -207,6 +387,41 @@ const styles = StyleSheet.create({
   calculateLabel: {    
     textAlign: 'center',
     backgroundColor: '#ADD8E6',
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  recordsBtn: {
+    width: 151,
+    height: 40,    
+    backgroundColor: '#FFD580'
+  },
+  recordsLabel: {
+    textAlign: 'center',
+    backgroundColor: '#FFD580',
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  goToRecordsBtn: {
+    width: 151,
+    height: 40,    
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFD580',
+  },
+  goToRecordsLabel: {
+    textAlign: 'center',
+    backgroundColor: '#FFD580',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  recommendationsBtn: {
+    width: 260,
+    height: 40,    
+    backgroundColor: '#90EE90'
+  },
+  recommendationsLabel: {
+    textAlign: 'center',
+    backgroundColor: '#90EE90',
     fontSize: 28,
     fontWeight: 'bold'
   },
@@ -273,6 +488,14 @@ const styles = StyleSheet.create({
     borderBottomColor: '#ffffff',
     borderBottomWidth: 2  
   },
+  homeContainersBtns: {
+    flexDirection: 'row',
+    gap: 30,
+    paddingTop: 100,
+    width: 335,
+    marginLeft: 'auto',
+    marginRight: 'auto', 
+  },
   cancelBtnForm: {
     justifyContent: 'center',
     width: 123,
@@ -292,7 +515,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   resultContainer: {
-    width: 280,
+    width: 280,  
     marginLeft: 'auto',
     marginRight: 'auto', 
   },
@@ -311,5 +534,30 @@ const styles = StyleSheet.create({
   resultDetailsContainer: {
     flexDirection: 'row',
     gap: 20
+  },
+  headerTable: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  recordsItems: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  titleHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 25,
+    paddingLeft: 45,
+    marginTop: 20
+  }, 
+  recordsItemList: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 45,
+    marginBottom: 10,
+    paddingLeft: 20
   }
 });
